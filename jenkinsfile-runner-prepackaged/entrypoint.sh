983e25e150d308da2bc8d85af1aafe00e0930de1
vijaypitla <vijaypitla@gmail.com>
2022-08-22 12:04:55 +0000

    Update entrypoint.sh

--- jenkinsfile-runner-prepackaged/entrypoint.sh
+++ jenkinsfile-runner-prepackaged/entrypoint.sh
@@ -23,15 +23,15 @@ run() {
   echo "jenkinsfile-runner: ${runner_ver}"
 
   echo
-  echo "java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins-${JENKINS_VERSION}.war --plugin-file ${pluginstxt} --plugin-download-directory=/usr/share/jenkins/ref/plugins"
-  java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins-${JENKINS_VERSION}.war --plugin-file ${pluginstxt} --plugin-download-directory=/usr/share/jenkins/ref/plugins
+  echo "java -jar /app/bin/jenkins-plugin-manager.jar --war ./jenkins-${JENKINS_VERSION}.war --plugin-file ${pluginstxt} --plugin-download-directory=/usr/share/jenkins/ref/plugins"
+  java -jar /app/bin/jenkins-plugin-manager.jar --war ./jenkins-${JENKINS_VERSION}.war --plugin-file ${pluginstxt} --plugin-download-directory=/usr/share/jenkins/ref/plugins
 
   echo
   ls -lrt /usr/share/jenkins/ref/plugins
 
   echo
-  echo "/app/bin/jenkinsfile-runner-launcher ${command} --jenkins-war=/app/jenkins-${JENKINS_VERSION} --file=${jenkinsfile} --plugins=/usr/share/jenkins/ref/plugins"
-  /app/bin/jenkinsfile-runner-launcher ${command} --jenkins-war=/app/jenkins-${JENKINS_VERSION} --file=${jenkinsfile} --plugins=/usr/share/jenkins/ref/plugins
+  echo "/app/bin/jenkinsfile-runner-launcher ${command} --jenkins-war=./jenkins-${JENKINS_VERSION} --file=${jenkinsfile} --plugins=/usr/share/jenkins/ref/plugins"
+  /app/bin/jenkinsfile-runner-launcher ${command} --jenkins-war=./jenkins-${JENKINS_VERSION} --file=${jenkinsfile} --plugins=/usr/share/jenkins/ref/plugins
 }
 
 run "${1}" "${2}" "${3}"
